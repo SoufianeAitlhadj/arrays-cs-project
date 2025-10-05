ce3e46c54dbaf53aaaf546697983f1334e9dbe66
SoufianeAitlhadj <aitlhadjsoufiane@gmail.com>
2025-10-05 23:07:51 +0000

    TP2>>Part3

--- src/main/java/samplearrays/BankAccount.java
+++ src/main/java/samplearrays/BankAccount.java
@@ -4,35 +4,63 @@ public class BankAccount {
 
     String name;
     double currentBalance;
-    //TO-DO: Initialize an Array with 1000 in size that stores Double called 'transactions' to keep track of the user's transactions
 
-    public BankAccount(String name, int startingBalance){
+    // 1. Initialize an Array with 1000 in size that stores Double called 'transactions'
+    double[] transactions = new double[1000];
+    int transactionCount = 0;
 
+
+    public BankAccount(String name, int startingBalance) {
+        this.name = name;
+        this.currentBalance = startingBalance;
+        transactions[transactionCount++] = startingBalance;
     }
 
-    public void deposit(double amount){
 
+    public void deposit(double amount) {
+        if (amount > 0) {
+            currentBalance += amount;
+            transactions[transactionCount++] = amount;
+            System.out.println(name + " deposited $" + amount + ". New balance: $" + currentBalance);
+        } else {
+            System.out.println("Invalid deposit amount: $" + amount + ". Deposit unsuccessful.");
+        }
     }
 
-    public void withdraw(double amount){
-
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= currentBalance) {
+            currentBalance -= amount;
+            transactions[transactionCount++] = -amount;
+            System.out.println(name + " withdrew $" + amount + ". New balance: $" + currentBalance);
+        } else {
+            System.out.println("Invalid withdrawal amount: $" + amount + ". Withdrawal unsuccessful.");
+        }
     }
 
-    public void displayTransactions(){
-
+    public void displayTransactions() {
+        System.out.println("\nTransaction history for " + name + ":");
+        for (int i = 0; i < transactionCount; i++) {
+            double transaction = transactions[i];
+            if (transaction > 0) {
+                System.out.println("Deposit: $" + transaction);
+            } else if (transaction < 0) {
+                System.out.println("Withdrawal: $" + (-transaction));
+            } else {
+                System.out.println("Starting Balance: $" + transaction);
+            }
+        }
     }
 
-    public void displayBalance(){
-
+    public void displayBalance() {
+        System.out.println("\nCurrent balance for " + name + ": $" + currentBalance);
     }
+
 
     public static void main(String[] args) {
 
         BankAccount john = new BankAccount("John Doe", 100);
 
         // ----- DO NOT CHANGE -----
-
-        //Testing..
         john.displayBalance();
         john.deposit(0.25);
         john.withdraw(100.50);
@@ -41,9 +69,6 @@ public class BankAccount {
         john.deposit(3000);
         john.displayTransactions();
         john.displayBalance();
-
         // ----- DO NOT CHANGE -----
-
     }
-
 }
